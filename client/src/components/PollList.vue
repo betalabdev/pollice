2cf998505190f6aff7efc36892f4976a82867900
manhtai <vo@manhtai.com>
2019-09-03 19:32:46 +0700

    Add copy code button

--- client/src/components/PollList.vue
+++ client/src/components/PollList.vue
@@ -2,17 +2,22 @@
     <div class="poll-view">
         <div class="poll-view__title">Poll list</div>
         <div class="poll-view__inner">
-            <div v-for="(question, index) in questions" :key="index" class="poll-view__question">
+            <div v-for="(question, index) in questions" :key="index" class="poll-view__code">
                 <h2>
                     {{index+1}}. {{ question.text }}&nbsp;
-                    <router-link :to="{ name: 'poll-detail', params: { questionId: question._id }}">&#9997;</router-link>&nbsp;
-                    <router-link :to="{ name: 'vote', params: { questionId: question._id }}">&#9993;</router-link>
+                    <router-link
+                        :to="{ name: 'poll-detail', params: { questionId: question._id }}"
+                    >&#9997;</router-link>&nbsp;
+                    <router-link :to="{ name: 'vote', params: { questionId: question._id }}">&#9995;</router-link>
                 </h2>
-                <input :value="'#poll=' + question._id" type="text" />
+                <input :id="question._id" :value="'#poll=' + question._id" type="text" />
+                <button @click="copy(question._id)">Copy code</button>
             </div>
         </div>
         <div class="poll-view__submit">
-            <router-link :to="{ name: 'poll-new' }"><button>New</button></router-link>
+            <router-link :to="{ name: 'poll-new' }">
+                <button>New</button>
+            </router-link>
         </div>
     </div>
 </template>
@@ -31,6 +36,14 @@ export default {
         api.getPolls((err, data) => {
             if (!err) this.questions = data
         })
+    },
+    methods: {
+        copy(questionId) {
+            let codeToCopy = document.getElementById(questionId)
+            codeToCopy.setAttribute('type', 'text')
+            codeToCopy.select()
+            document.execCommand('copy')
+        },
     },
 }
 </script>
