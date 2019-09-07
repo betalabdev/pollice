38cd829cfa680474f8891771cdafaa77b5888a3b
manhtai <vo@manhtai.com>
2019-09-07 15:46:26 +0700

    Done deployment

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -1,6 +1,6 @@
 export default {
     env: process.env.NODE_ENV || "local",
-    clientUrl: process.env.CLIENT_URL || "https://127.0.0.1:8080",
-    serverUrl: process.env.SERVER_URL || "https://127.0.0.1:3000",
-    clientId: process.env.CLIENT_ID || "481318809056-v7u1644mckbkhllsr6hi6igflsp3ag52.apps.googleusercontent.com",
+    clientUrl: process.env.VUE_APP_CLIENT_URL || "https://127.0.0.1:8080",
+    serverUrl: process.env.VUE_APP_SERVER_URL || "https://127.0.0.1:3000",
+    clientId: process.env.VUE_APP_CLIENT_ID || "481318809056-v7u1644mckbkhllsr6hi6igflsp3ag52.apps.googleusercontent.com",
 }
