0a88b9420fc40d60807e580353f6786ccc6865ad
manhtai <vo@manhtai.com>
2019-09-07 22:52:01 +0700

    Add color

--- client/src/components/Result.vue
+++ client/src/components/Result.vue
@@ -32,6 +32,15 @@ export default {
         return {
             questionId: null,
             question: {},
+            colors: [
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(255, 159, 64, 0.8)',
+                'rgba(255, 205, 86, 0.8)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(153, 102, 255, 0.8)',
+                'rgba(201, 203, 207, 0.8)',
+            ],
         }
     },
     computed: {
@@ -79,7 +88,7 @@ export default {
                 datasets: [
                     {
                         label: result.text,
-                        backgroundColor: '#f87979',
+                        backgroundColor: this.colors,  // FIXME: Multiply this
                         data: result.answers.map(a => a.votes),
                     },
                 ],
